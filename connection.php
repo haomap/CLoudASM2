42263b26f76caded197d52a830afcb87baea6879
haomap <haonngcc200209@fpt.edu.vn>
2023-06-24 14:56:58 +0700

    hihi

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     // $conn = mysqli_connect('localhost','root','','cloudasm') 
     // or die("Cannot connect database".mysqli_connect_error());
 
-    $conn = mysqli_connect("l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","kzz6wrcbj4kyflfq","swp43hheb82lm44z","qvma6i9xvv07wovb") 
+    $conn = mysqli_connect("co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","qjyikc5pc19x620g","dowlcekrckr1rxj2","z79rnbmx4isefzu0") 
     or die("Cannot connect database".mysqli_connect_error());
 
     mysqli_query($conn, 'SET NAMES "utf8"')
